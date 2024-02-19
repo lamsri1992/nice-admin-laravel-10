c1e5e8a7ab8814b557bb29db62aeaea77e00b47d
SATiYA <satiya@MACBOOKs-MacBook-Pro.local>
2024-02-19 15:32:12 +0700

    first commit

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,12 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        <a href="https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/" target="_blank">
+            Nice Admin
+        </a>
+    </div>
+    <div class="credits">
+        Midify On Laravel 10 By - SATiYA.CH
+    </div>
+</footer>
+<!-- End Footer -->
